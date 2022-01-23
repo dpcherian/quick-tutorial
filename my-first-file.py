65977de2aff9a2ae3c842c45531781134e06067f
Philip Cherian <dpcherian@gmail.com>
2022-01-23 15:31:42 +0530

    Allowed accepting  user input
    This is a longer line that describes what I did.

--- my-first-file.py
+++ my-first-file.py
@@ -2,6 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-n = np.random.randint(10)
+# This is a random comment
+
+
+print("This is a program that prints a random number between 1 and a number of your choice")
+
+print("Insert maximum number")
+max_n = input()
+
+n = np.random.randint(max_n)
 
 print(n)
+
+# End of the program? Add a conclusion?
+# Maybe not
